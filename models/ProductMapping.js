25e01f7bb55b568f410b7ef5f8f974288f71bfc7
Suryabehara-1995 <surya.behara@a2znaturopathy.com>
2025-04-04 15:44:32 +0530

    Update ProductMapping.js

--- models/ProductMapping.js
+++ models/ProductMapping.js
@@ -4,6 +4,7 @@ const productMappingSchema = new mongoose.Schema({
   productID: { type: String, required: true, unique: true },
   updatedID: { type: String, required: true },
   productName: { type: String, required: true },
+  sku: { type: String, required: true }, // Add SKU field
 });
 
 module.exports = mongoose.model("ProductMapping", productMappingSchema);
