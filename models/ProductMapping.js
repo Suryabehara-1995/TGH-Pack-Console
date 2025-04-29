96bea3b597ae2b44c4d9e38be03c1c450ae71878
Suryabehara-1995 <surya.behara@a2znaturopathy.com>
2025-04-29 16:37:27 +0530

    Update ProductMapping.js

--- models/ProductMapping.js
+++ models/ProductMapping.js
@@ -1,10 +1,14 @@
+// productMappingSchema.js
 const mongoose = require("mongoose");
 
 const productMappingSchema = new mongoose.Schema({
   productID: { type: String, required: true, unique: true },
   updatedID: { type: String, required: true },
   productName: { type: String, required: true },
-  sku: { type: String, required: true }, // Add SKU field
+  sku: { type: String, required: true },
+  productLocation: { type: String, default: "" },
+  productCategory: { type: String, default: "" },
+  imageUrl: { type: String, default: "" }, // New field for image URL
 });
 
 module.exports = mongoose.model("ProductMapping", productMappingSchema);
